e09b92463c9e6b6120e5e7d95c4e5fe94b09939c
tomryan01 <tomryan01@outlook.com>
2021-01-29 12:12:32 +0000

    Setup basic structure for grabber, detection and gps classes

---
+++ src/controllers/main/Detection.py
@@ -0,0 +1,12 @@
+from MyRobot import MyRobot
+
+class Detection(MyRobot):
+
+    def block_in_sight(self):
+        pass
+
+    def block_colour(self):
+        pass
+
+    def block_distance(self):
+        pass
---
+++ src/controllers/main/Grabber.py
@@ -0,0 +1,12 @@
+from MyRobot import MyRobot
+
+class Grabber(MyRobot):
+
+    def pick_up(self):
+        pass
+
+    def put_down(self):
+        pass
+
+    def block_picked_up(self):
+        pass
