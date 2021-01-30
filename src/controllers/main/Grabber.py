a1b2f149b019978dd4d72aa4aa1f9c17e2c5d5b7
Rupert Annison <peartreesurfr@gmail.com>
2021-01-30 23:14:52 +0000

    Arms move and changeattribute armsPosition

--- src/controllers/main/Grabber.py
+++ src/controllers/main/Grabber.py
@@ -3,10 +3,31 @@ from MyRobot import MyRobot
 class Grabber(MyRobot):
 
     def pick_up(self):
-        pass
+        #set target position as 2 pi radians so block is picked up
+        for i in range(self.numArms):
+            self.arms[i].setVelocity(2)
+            self.arms[i].setPosition(1.57)
+        
+        #arms being moved up
+        self.armsPosition = 2
+
+        #use count to determin if arms in final position and change position to 1, reset count
+        self.armCount += 1
+        if self.armCount == 20:
+            self.armsPosition = 1
+            self.armCount = 0
 
     def put_down(self):
-        pass
+        #set target position as 0 so arms down
+        for i in range(self.numArms):
+            self.arms[i].setVelocity(2)
+            self.arms[i].setPosition(0)
+        
+        #arms being moved down
+        self.armsPosition = 3
 
-    def block_picked_up(self):
-        pass
+        #use count to determin if arms in final position and change position to 0, reset count
+        self.armCount += 1
+        if self.armCount == 20:
+            self.armsPosition = 0
+            self.armCount = 0
