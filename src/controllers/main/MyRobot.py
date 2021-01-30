ffb31fded95b4c8b39e153ff23277f9046efc33f
Rupert Annison <peartreesurfr@gmail.com>
2021-01-30 14:50:20 +0000

    added gps sensor set up, and enabling of others

--- src/controllers/main/MyRobot.py
+++ src/controllers/main/MyRobot.py
@@ -13,6 +13,7 @@ class MyRobot:
         self.arms = []
         self.distanceSensors = []
         self.colourSensors = []
+        self.gpsSensors = []
 
         #constants for how many of each device
         #TO CHANGE THE NUMBER OF DEVICES THESE ARE THE ONLY NUMBER THAT SHOULD BE CHANGED WITHIN THIS CLASS
@@ -20,6 +21,10 @@ class MyRobot:
         self.numArms = 2
         self.numDistanceSensors = 1
         self.numColourSensors = 1
+        self.numGPS = 2
+
+        #set origin/start point for robot
+        self.origin = [1, 1]
 
     #The following methods are used to set up the devices for a robot
     #Each method begins by asserting the length of names in the list
@@ -38,15 +43,23 @@ class MyRobot:
         for i in range(2):
             self.arms.append(self.robot.getDevice(armNames[i]))
 
-    def setDistanceSensors(self, distanceSensors):
+    def setDistanceSensors(self, distanceSensors, TimeStep):
         assert len(distanceSensors) == self.numDistanceSensors
         for i in range(1):
             self.distanceSensors.append(self.robot.getDevice(distanceSensors[i]))
+            self.distanceSensors[i].enable(TimeStep)
 
-    def setColourSensors(self, colourSensors):
+    def setColourSensors(self, colourSensors, TimeStep):
         assert len(colourSensors) == self.numColourSensors
         for i in range(1):
             self.colourSensors.append(self.robot.getDevice(colourSensors[i]))
+            self.colourSensors[i].enable(TimeStep)
+
+    def setGPS(self, gpsSensors, TimeStep):
+        assert len(gpsSensors) == self.numGPS
+        for i in range(2):
+            self.gpsSensors.append(self.robot.getDevice(gpsSensors[i]))
+            self.gpsSensors[i].enable(TimeStep)
 
     def reset(self):
         self.wheels[0].setVelocity(0)
