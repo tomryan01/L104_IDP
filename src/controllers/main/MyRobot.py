79372c1bd29064df087859533ffcfb5b0735e7be
Rupert Annison <peartreesurfr@gmail.com>
2021-01-30 19:06:26 +0000

    put numXXX as limit for initialising the sensors this will allow us to add extras in future, without changing the code

--- src/controllers/main/MyRobot.py
+++ src/controllers/main/MyRobot.py
@@ -31,33 +31,32 @@ class MyRobot:
     #Is the number of sensors specified
     def setWheels(self, wheelNames):
         assert len(wheelNames) == self.numWheels
-        for i in range(2):
+        for i in range(numWheels):
             self.wheels.append(self.robot.getDevice(wheelNames[i]))
             
-        #wheel positions will be defined by velocity
-        self.wheels[0].setPosition(float('inf'))
-        self.wheels[1].setPosition(float('inf'))
+            #wheel positions will be defined by velocity
+            self.wheels[i].setPosition(float('inf'))
 
     def setArms(self, armNames):
         assert len(armNames) == self.numArms
-        for i in range(2):
+        for i in range(numArms):
             self.arms.append(self.robot.getDevice(armNames[i]))
 
     def setDistanceSensors(self, distanceSensors, timeStep):
         assert len(distanceSensors) == self.numDistanceSensors
-        for i in range(1):
+        for i in range(numDistanceSensors):
             self.distanceSensors.append(self.robot.getDevice(distanceSensors[i]))
             self.distanceSensors[i].enable(timeStep)
 
     def setColourSensors(self, colourSensors, timeStep):
         assert len(colourSensors) == self.numColourSensors
-        for i in range(1):
+        for i in range(numColourSensors):
             self.colourSensors.append(self.robot.getDevice(colourSensors[i]))
             self.colourSensors[i].enable(timeStep)
 
     def setGps(self, gpsSensors, timeStep):
         assert len(gpsSensors) == self.numGps
-        for i in range(2):
+        for i in range(numGps):
             self.gpsSensors.append(self.robot.getDevice(gpsSensors[i]))
             self.gpsSensors[i].enable(timeStep)
 
