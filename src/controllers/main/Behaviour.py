b286acbc7565523aa9da937b4034e688946ee244
tomryan01 <tomryan01@outlook.com>
2021-02-02 21:22:15 +0000

    Some general changes to conditions and added checking block colour

--- src/controllers/main/Behaviour.py
+++ src/controllers/main/Behaviour.py
@@ -15,7 +15,7 @@ class Behaviour(Detection, Drive, Gps, Grabber):
     def findBlocks(self):
         #main block finding algorithm
         #If there is no block within range, spin
-        if not(self.block_in_sight()) or (abs(self.direction_from_start()) < 1.57 and self.armsPosition == 0):
+        if (not(self.block_in_sight()) and self.distance_from_start() < 0.2) or (abs(self.direction_from_start() < 1.57 and self.armsPosition == 0)):
             print("a")
             self.spin(1, 1)
         #When block seen, drive forwards unless block close enough to pick up
@@ -23,7 +23,7 @@ class Behaviour(Detection, Drive, Gps, Grabber):
             print("b")
             self.forwards(3)
         #If arms are down, or moving up then pick up the block and record its position
-        elif self.armsPosition == 0 or self.armsPosition == 2:
+        elif (self.armsPosition == 0 or self.armsPosition == 2 and self.block_colour() > 70):
             print("c")
             self.pick_up()
             self.blockOriginalDistance = self.distance_from_start()
@@ -31,8 +31,13 @@ class Behaviour(Detection, Drive, Gps, Grabber):
         elif not(self.distance_from_start() < 0.7*self.blockOriginalDistance):
             print("d")
             self.backwards(3)
+        #When past 30% of way, check to see if robot still has block
+        elif self.block_colour() < 70:
+            # this will drop the arms, but not put anything down as it doesn't have the block
+            # it will then re-initiate the pickup sequence
+            self.put_down()
         #When past 30% of the way, ensure robot is facing origin
-        elif not(abs(self.direction_from_start()) < 0.1):
+        elif not(abs(self.direction_from_start()) < 0.1) and not(self.armsPosition == 0) :
             print("e")
             #spin the correct direction, positive is turn right
             #important for later corrections
@@ -41,7 +46,7 @@ class Behaviour(Detection, Drive, Gps, Grabber):
             else:
                 self.spin(1, -1)
         #Keep going forward until it returns to start
-        elif self.distance_from_start() > 0.01:
+        elif self.distance_from_start() > 0.05:
             print("f")
             self.forwards(3)
         #When at the start put block down until arms in down position
