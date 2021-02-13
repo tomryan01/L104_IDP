cab7dffdc2f9319792f5f5e726caaac678380bb1
tomryan01 <tomryan01@outlook.com>
2021-02-13 17:24:40 +0000

    call of looking_in_list checks for none not bool

--- src/controllers/main/Behaviour.py
+++ src/controllers/main/Behaviour.py
@@ -30,7 +30,7 @@ class Behaviour(Detection, Drive, Gps, Grabber, Communication):
         #must call this every time
         #self.emit_position()
         #TODO: emit position again
-        
+
         #if not called then reciver queue fills up with old data
         looking_at_friend = self.looking_at_my_friend()
 
@@ -38,7 +38,7 @@ class Behaviour(Detection, Drive, Gps, Grabber, Communication):
         if(self.state == [0,1]):
             self.spin(1, 1)
             if(self.block_in_sight() and not(self.coordinate_in_my_box(self.coordinate_looking_at())) and (self.get_distance() < 1390)):
-                if(not(self.looking_in_list(self.blockLocations)) and not(self.distance_inside_friend_corner())):
+                if(self.looking_in_list(self.blockLocations) == None and not(self.distance_inside_friend_corner())):
                     if(not(looking_at_friend)):
                         self.state[1] += 1
         #initially go forwards towards block
